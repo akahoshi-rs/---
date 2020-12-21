44ab6626828a979bbb67dddd3a558c6516d74215
akahoshi-rs <72480139+akahoshi-rs@users.noreply.github.com>
2020-12-22 00:38:52 +0900

    m

--- OPmodal.js
+++
@@ -1,4 +0,0 @@
-const Main = document.getElementById('Main');
-const Sub1 = document.getElementById('Sub-1');
-const Sub2 = document.getElementById('Sub-2');
-const Sub3 = document.getElementById('Sub-3');
