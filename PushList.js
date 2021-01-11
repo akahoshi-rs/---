ff6b77ebfae2704c1fe038f7fb1efad01f492add
akahoshi-rs <72480139+akahoshi-rs@users.noreply.github.com>
2021-01-12 01:58:51 +0900

    個別の値の取得を組み直すため一時的に機能を停止しています。

--- PushList.js
+++ PushList.js
@@ -10,12 +10,12 @@ function flexTextarea(el) {
 document.querySelectorAll('.FlexTextarea').forEach(flexTextarea)
 /////////////////////////////////////////
 
+//右クリックの非表示
+document.oncontextmenu = function () { return false; }
 //second-を追加していく親要素
 const ContentsArea = document.getElementsByClassName('contents')[0];
 //textareaに入れられた文字(.value)
 const Contents = document.getElementById('flexTextarea__textarea');
-//.valueを表示する場所
-const Log = document.getElementsByClassName('output_div')[0];
 //previewコンテナ
 const Preview = document.getElementsByClassName('preview')[0];
 //送信ボタン(shift+enter)
@@ -27,27 +27,8 @@ const CloseWindow = document.getElementById('batu');
 //TermList
 const TermList = document.querySelectorAll('.ListDetails ol');
 const BlankList = document.getElementById('blank-list');
-
-//second-termのdivをつくる
-function clone() {
-    const TermBox = document.createElement('div');
-    TermBox.draggable = true;
-    TermBox.className = 'second-term drag-drop';
-    ContentsArea.appendChild(TermBox);
-}
 //second-termの取得(初期読み込み)
 const TermBoxes = document.querySelectorAll(".second-term");
-
-//TermListのliの追加
-function ListPush() {
-    const ListPush = document.createElement('li');
-    ListPush.className = 'Term-List';
-    BlankList.appendChild(ListPush);
-};
-//ListPushの取得
-const ListPushes = document.querySelectorAll('#TermList .Term-List');
-//_Textcontent.valueをlistに代入
-let _ContentsTextArray = [];
 
 //first-termの取得
 const FirstTerm = document.getElementById('first-term');
@@ -79,8 +60,24 @@ CloseWindow.addEventListener('mouseup', function () {
     }
 });
 
-//右クリックの非表示
-document.oncontextmenu = function () { return false; }
+//second-termのdivをつくる
+function clone() {
+    const TermBox = document.createElement('div');
+    TermBox.draggable = true;
+    TermBox.className = 'second-term drag-drop';
+    ContentsArea.appendChild(TermBox);
+}
+
+//TermListのliの追加
+function ListPush() {
+    const ListPush = document.createElement('li');
+    ListPush.className = 'Term-List';
+    BlankList.appendChild(ListPush);
+};
+//ListPushの取得
+const ListPushes = document.querySelectorAll('#TermList .Term-List');
+//_Textcontent.valueをlistに代入
+let _ContentsTextArray = [];
 
 //オリジナル コンテキストメニュー
 let con = document.getElementById('conmenu');
@@ -93,123 +90,126 @@ const innyou = document.getElementById('innyou');
 function WordPush() {
     //_ContentsTextArrayに追加する
     _ContentsTextArray[_ContentsTextArray.length] = [Contents.value];
-    console.log(_ContentsTextArray);
     //second-termに追加する
     clone();
     const TermBoxes = document.querySelectorAll(".second-term");
     //TermListに追加する
     ListPush();
     const ListPushes = document.querySelectorAll('#TermList .Term-List');
-
+    // console.log(_ContentsTextArray);
+    // console.log(ListPushes);
+    // console.log(TermBoxes);
     //代入するやつ
+
+    //listとTermのvalueをarrayで比べる
     for (let i = 0; i < _ContentsTextArray.length; i++) {
+        //LISTとTERMのVALUEをARRAYから持ってくる
         ListPushes[i].innerHTML = _ContentsTextArray[i];
         TermBoxes[i].innerHTML = _ContentsTextArray[i];
-        //一時的な代入
-        Log.textContent = _ContentsTextArray[i];
+        //Previewコンテナに一時的に表示
         Preview.textContent = _ContentsTextArray[i];
 
-        ListPushes[i].addEventListener('click', function () {
-            Contents.value = ListPushes[i].innerHTML;
-            Preview.textContent = ListPushes[i].innerHTML;
-        });
+        //         ListPushes[i].addEventListener('click', function () {
+        //             Contents.value = ListPushes[i].innerHTML;
+        //             Preview.textContent = ListPushes[i].innerHTML;
+        //         });
 
-        TermBoxes[i].addEventListener('contextmenu', function (e) {
-            Preview.textContent = TermBoxes[i].innerHTML;
-            //マウスの位置を使ってスタイルを設定する
-            con.style.left = e.pageX + 'px';
-            con.style.top = e.pageY + 'px';
-            //メニューをblockで表示
-            con.classList.add('show');
-            TermBoxes[i].classList.add('delete');
-            ListPushes[i].classList.add('delete-list');
+        //         TermBoxes[i].addEventListener('contextmenu', function (e) {
+        //             Preview.textContent = TermBoxes[i].innerHTML;
+        //             //マウスの位置を使ってスタイルを設定する
+        //             con.style.left = e.pageX + 'px';
+        //             con.style.top = e.pageY + 'px';
+        //             //メニューをblockで表示
+        //             con.classList.add('show');
+        //             TermBoxes[i].classList.add('delete');
+        //             ListPushes[i].classList.add('delete-list');
 
-            const TermDelete = document.getElementsByClassName('delete');
-            const ListDelete = document.getElementsByClassName('delete-list');
-            let innerTerm = TermDelete[0].innerHTML;
+        //             const TermDelete = document.getElementsByClassName('delete');
+        //             const ListDelete = document.getElementsByClassName('delete-list');
+        //             let innerTerm = TermDelete[0].innerHTML;
 
-            innyou.addEventListener('click', function () {
-                Contents.value = TermBoxes[i].innerHTML;
-            });
+        //             innyou.addEventListener('click', function () {
+        //                 Contents.value = TermBoxes[i].innerHTML;
+        //             });
 
-            Sakuzyo.addEventListener('click', function () {
-                let result = _ContentsTextArray.filter(function (item, index, arr) {
-                    arr.pop()
-                    return item.innerHTML !== innerTerm;
-                });
-                result = _ContentsTextArray;
-                if (ListDelete.length > 0) {
-                    ListDelete[0].remove();
-                }
-                if (TermDelete.length > 0) {
-                    TermDelete[0].remove();
-                }
-            });
-
-
-            //左クリックで非表示に変更
-            body.addEventListener('click', function () {
-                if (con.classList.contains('show')) {
-                    //非表示に戻す
-                    con.classList.remove('show');
-                }
-                if (TermBoxes[i].classList.contains('delete')) {
-                    TermBoxes[i].classList.remove('delete');
-                }
-                if (ListPushes[i].classList.contains('delete-list')) {
-                    ListPushes[i].classList.remove('delete-list');
-                }
-            });
-        });
-
-        ListPushes[i].addEventListener('contextmenu', function (e) {
-            Preview.textContent = TermBoxes[i].innerHTML;
-            //マウスの位置を使ってスタイルを設定する
-            con.style.left = e.pageX + 'px';
-            con.style.top = e.pageY + 'px';
-            //メニューをblockで表示
-            con.classList.add('show');
-            TermBoxes[i].classList.add('delete');
-            ListPushes[i].classList.add('delete-list');
-
-            const TermDelete = document.getElementsByClassName('delete');
-            const ListDelete = document.getElementsByClassName('delete-list');
-            let innerTerm = TermDelete[0].innerHTML;
-
-            innyou.addEventListener('click', function () {
-                Contents.value = TermBoxes[i].innerHTML;
-            });
-
-            Sakuzyo.addEventListener('click', function () {
-                let result = _ContentsTextArray.filter(function (item, index, arr) {
-                    arr.pop()
-                    return item.innerHTML !== innerTerm;
-                });
-                result = _ContentsTextArray;
-                if (ListDelete.length > 0) {
-                    ListDelete[0].remove();
-                }
-                if (TermDelete.length > 0) {
-                    TermDelete[0].remove();
-                }
-            });
+        //             Sakuzyo.addEventListener('click', function () {
+        //                 let result = _ContentsTextArray.filter(function (item, index, arr) {
+        //                     arr.pop()
+        //                     return item.innerHTML !== innerTerm;
+        //                 });
+        //                 result = _ContentsTextArray;
+        //                 if (ListDelete.length > 0) {
+        //                     ListDelete[0].remove();
+        //                 }
+        //                 if (TermDelete.length > 0) {
+        //                     TermDelete[0].remove();
+        //                 }
+        //             });
 
 
-            //左クリックで非表示に変更
-            body.addEventListener('click', function () {
-                if (con.classList.contains('show')) {
-                    //非表示に戻す
-                    con.classList.remove('show');
-                }
-                if (TermBoxes[i].classList.contains('delete')) {
-                    TermBoxes[i].classList.remove('delete');
-                }
-                if (ListPushes[i].classList.contains('delete-list')) {
-                    ListPushes[i].classList.remove('delete-list');
-                }
-            });
+        //             //左クリックで非表示に変更
+        //             body.addEventListener('click', function () {
+        //                 if (con.classList.contains('show')) {
+        //                     //非表示に戻す
+        //                     con.classList.remove('show');
+        //                 }
+        //                 if (TermBoxes[i].classList.contains('delete')) {
+        //                     TermBoxes[i].classList.remove('delete');
+        //                 }
+        //                 if (ListPushes[i].classList.contains('delete-list')) {
+        //                     ListPushes[i].classList.remove('delete-list');
+        //                 }
+        //             });
+        //         });
 
-        });
+        //         ListPushes[i].addEventListener('contextmenu', function (e) {
+        //             Preview.textContent = TermBoxes[i].innerHTML;
+        //             //マウスの位置を使ってスタイルを設定する
+        //             con.style.left = e.pageX + 'px';
+        //             con.style.top = e.pageY + 'px';
+        //             //メニューをblockで表示
+        //             con.classList.add('show');
+        //             TermBoxes[i].classList.add('delete');
+        //             ListPushes[i].classList.add('delete-list');
+
+        //             const TermDelete = document.getElementsByClassName('delete');
+        //             const ListDelete = document.getElementsByClassName('delete-list');
+        //             let innerTerm = TermDelete[0].innerHTML;
+
+        //             innyou.addEventListener('click', function () {
+        //                 Contents.value = TermBoxes[i].innerHTML;
+        //             });
+
+        //             Sakuzyo.addEventListener('click', function () {
+        //                 let result = _ContentsTextArray.filter(function (item, index, arr) {
+        //                     arr.pop()
+        //                     return item.innerHTML !== innerTerm;
+        //                 });
+        //                 result = _ContentsTextArray;
+        //                 if (ListDelete.length > 0) {
+        //                     ListDelete[0].remove();
+        //                 }
+        //                 if (TermDelete.length > 0) {
+        //                     TermDelete[0].remove();
+        //                 }
+        //             });
+
+
+        //             //左クリックで非表示に変更
+        //             body.addEventListener('click', function () {
+        //                 if (con.classList.contains('show')) {
+        //                     //非表示に戻す
+        //                     con.classList.remove('show');
+        //                 }
+        //                 if (TermBoxes[i].classList.contains('delete')) {
+        //                     TermBoxes[i].classList.remove('delete');
+        //                 }
+        //                 if (ListPushes[i].classList.contains('delete-list')) {
+        //                     ListPushes[i].classList.remove('delete-list');
+        //                 }
+        //             });
+
+        //         });
 
         //移動の取得
         TermBoxes[i].addEventListener('mousedown', function () {
@@ -249,7 +249,6 @@ function WordPush() {
                     ListPushes[i].style.boxShadow = "inset 0 2px 6px" + Sub3Color.value;
                 };
             }
-            console.log(_ContentsTextArray);
             setTimeout(function () {
                 if (TermBoxes[i].classList.contains('appending')) {
                     TermBoxes[i].classList.remove('appending');
@@ -283,6 +282,7 @@ function WordPush() {
     //textboxを空にする
     Contents.value = "";
 }
+
 
 Contents.addEventListener("keydown", function () {
     if (event.shiftKey == true) {
